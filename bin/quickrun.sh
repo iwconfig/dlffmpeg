e814c0ef385acd8fe07e16bc00e8ff71c14785c3
iwconfig <snelhingst@gmail.com>
2017-02-21 23:42:51 +0000

    quickstart.sh: use correct url depending on os
    	Note to self: don't code and not sleep. It will kill me.

--- bin/quickrun.sh
+++ bin/quickrun.sh
@@ -1,33 +1,24 @@
 #!/bin/bash
-arch=$(uname -m)
-os=$(uname)
+echo "Hello, this installs ffmpeg to the system default path (/usr/local/bin)."
+arch=$(uname -m | tr "[:upper:]" "[:lower:]")
+os=$(uname | tr "[:upper:]" "[:lower:]")
 client=$(command -v curl || command -v wget)
 url=https://api.github.com/repos/iwconfig/dlffmpeg/releases/latest
 tmp=/tmp/QUICK_DLFFMPEG
 if [[ "$arch" == *arm* ]]; then
-    [[ ${arch//[^0-9]/} < 7 ]] && echo "Sorry, only ARM version >= armv7l is supported (armhf). You are using $arch." && exit 1
-    arch='armhf'
-fi
-[[ -x $tmp ]] && $tmp && rm $tmp && exit 0 || [[ ! -x $tmp ]] && : || exit 1
+		[[ ${arch//[^0-9]/} -lt 7 ]] && echo "Sorry, only ARM version >= armv7l is supported (armhf). You are using $arch." && exit 1
+		arch='armhf'; fi
+[[ -f $tmp ]] && rm $tmp
 if [[ -x $client ]]; then
-    if [[ "$client" == *wget ]]; then
-        client+=" -q -O -"
-    else
-        client+=" -# -L"
-    fi
-    IFS=' ' read -r -a github_urls <<< $($client -s "$url" | grep 'browser_' | cut -d\" -f4)
-    for url in "${github_urls[@]}"; do
-        if [[ ! "$arch" == "armhf" ]]; then
-            [[ "$url" == *linux* ]] && [[ "$url" != *armhf* ]] && url=$url || \
-            [[ "$url" == *macosx* ]] && url=$url && break
-        else
-            [[ "$url" == *armhf* ]] && url=$url && break
-        fi
-    done
-    echo Downloading $url
-    $client "$url" >$tmp && chmod +x $tmp
-    $tmp && rm $tmp
+		[[ "$client" == *wget ]] && client+=" -q -O -" || client+=" -# -L"
+		IFS=' ' read -r -a github_urls <<< "$($client -s "$url" | grep 'browser_' | cut -d\" -f4 | tr '\n' ' ')"
+		[[ "$os" == "darwin" ]] && for url in "${github_urls[@]}"; do [[ "$url" == *macosx* ]] && url=$url && break; done
+		if [[ "$os" == "linux" ]]; then
+				if [[ "$arch" == "armhf" ]]; then
+						for url in "${github_urls[@]}"; do [[ "$url" == *armhf* ]] && url=$url && break; done
+				else
+						for url in "${github_urls[@]}"; do [[ "$url" == *linux* ]] && url=$url && break; done; fi; fi
+		echo Downloading "$url"
+		$client "$url" >$tmp && chmod +x $tmp && sudo $tmp && rm $tmp
 else
-    echo Please install curl or wget.
-    exit 1
-fi
+		echo Please install curl or wget. && exit 1; fi
